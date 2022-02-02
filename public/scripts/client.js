7af178f53e9841318bf39a539814e69a25fdc4e0
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-02 13:51:20 -0500

    implemented createTweetElement function for hardcoded data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,55 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+const createTweetElement = tweetData => {
+  const tweetHTML = `<article class="tweet">
+  <header>
+    <div class="tweet-header">
+      <div class="full-name-tweeter">
+        <img src="${tweetData.user.avatars}" alt="">
+        <p>${tweetData.user.name}</p>
+      </div>
+    </div>
+    <p>${tweetData.user.handle}</p>
+  </header>
+  <div class="tweet-content">
+    <p>${tweetData.content.text}</p>
+  </div>
+  <footer>
+    <div class="tweet-footer">
+      <div class="date">
+        <p>10 days ago</p>
+      </div>
+      <div class="tweet-actions">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </div>
+  </footer>
+</article>`;
+  return tweetHTML;
+};
+
+const $tweet = createTweetElement(tweetData);
+
+
+console.log($tweet);
+
+$(document).ready(() => {
+  console.log('document ready')
+  $('#tweets-container').append($tweet);
+});
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $('#tweet-text').on('input', function() {
     const count = (140 - $(this).val().length);
-    $('.counter').text(count.toString(10));
+    const counter = $(this).siblings().find("output");
+    counter.text(count.toString(10));
     if (count < 0) {
-      $('.counter').addClass('over-character-limit');
+      counter.addClass('over-character-limit');
     } else {
-      $('.counter').removeClass('over-character-limit');
+      counter.removeClass('over-character-limit');
     }
   });
 });
