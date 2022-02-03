50976120f2110cd998d3b783cd5948f427115ec1
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-02 21:42:25 -0500

    Clear textarea in form after successful submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,6 +30,7 @@ const onSubmit = function(event) {
     $.post('/tweets/', data)
       .then(() => {
         loadTweets();
+        $('#tweet-text').val('');
       });
   }
 };
@@ -40,31 +41,6 @@ const loadTweets = () => {
       renderTweets(data);
     });
 };
-
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1643654725453
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1643741125453
-  }
-];
 
 const createTweetElement = tweetData => {
   const $tweet = `<article class="tweet">
